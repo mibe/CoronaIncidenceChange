114c92dd310f7640984ebc6fd04b70d8dc0182b8
Michael Bemmerl <mail@mx-server.de>
2021-07-16 00:23:29 +0200

    Display the incidence in the table.
    And calculate everything in one for loop only.

--- CoronaIncidenceChange.py
+++ CoronaIncidenceChange.py
@@ -5,77 +5,14 @@ import urllib.request
 import xml.etree.ElementTree as ET
 import re
 
+# https://www.ecdc.europa.eu/en/publications-data/data-daily-new-cases-covid-19-eueea-country
 data_url = 'https://opendata.ecdc.europa.eu/covid19/nationalcasedeath_eueea_daily_ei/csv/data.csv'
 svg_url = 'https://upload.wikimedia.org/wikipedia/commons/0/05/Blank_map_of_Europe_%28without_disputed_regions%29.svg'
 days = 7
 red = '#c00000'
 green = '#00c000'
 
-def get_population(country):
-    if country == 'AT':
-        return 8932664
-    elif country == 'BE':
-        return 11492641
-    elif country == 'BG':
-        return 6951482
-    elif country == 'CY':
-        return 848300
-    elif country == 'CZ':
-        return 10700000
-    elif country == 'DE':
-        return 83155031
-    elif country == 'DK':
-        return 5840045
-    elif country == 'EE':
-        return 1300000
-    elif country == 'GR':
-        return 10718565
-    elif country == 'ES':
-        return 47100000
-    elif country == 'FI':
-        return 5536146
-    elif country == 'FR':
-        return 67422000
-    elif country == 'HR':
-        return 4058165
-    elif country == 'HU':
-        return 9769526
-    elif country == 'IE':
-        return 4900000
-    elif country == 'IS':
-        return 361000
-    elif country == 'IT':
-        return 60026546
-    elif country == 'LI':
-        return 38650
-    elif country == 'LT':
-        return 3029118
-    elif country == 'LU':
-        return 626108
-    elif country == 'LV':
-        return 1900000
-    elif country == 'MT':
-        return 520700
-    elif country == 'NL':
-        return 17474693
-    elif country == 'NO':
-        return 5391369
-    elif country == 'PL':
-        return 37798655
-    elif country == 'PT':
-        return 10295909
-    elif country == 'RO':
-        return 19400000
-    elif country == 'SE':
-        return 10327589
-    elif country == 'SI':
-        return 2081912
-    elif country == 'SK':
-        return 5457873
-    else:
-        return 0
-
-def get_cases(data, country, backlook):
+def get_cases(country, backlook):
     if country == 'GR':
         country = 'EL'
     temp = data.query('geoId == "{0}"'.format(country))
@@ -83,7 +20,7 @@ def get_cases(data, country, backlook):
     
     return temp.cases.sum()
 
-def get_countries_in_dataset(data):
+def get_countries_in_dataset():
     list = data.geoId.drop_duplicates().to_list()
     
     if 'EL' in list:
@@ -97,35 +34,39 @@ def get_countries_in_dataset(data):
 def calc_incidence(cases, pop):
     return cases / pop * 100000
 
-urllib.request.urlretrieve(data_url, "data.csv")
+#urllib.request.urlretrieve(data_url, "data.csv")
 urllib.request.urlretrieve(svg_url, "map.svg")
 
 data = pd.read_csv('data.csv')
+pop_data = pd.read_csv('countries.csv')
 
-countries = get_countries_in_dataset(data)
-changes = {}
+countries = get_countries_in_dataset()
+
+svg = ET.parse('map.svg')
+root_node = svg.getroot()
+
+html = open('results.html', 'w')
+html.write('<!doctype html>\n')
+html.write('<html lang="en">\n')
+html.write('<head><title>Incidence change in the EU</title><script src="sorttable.js"></script><style>td { border: 1px solid #a2a9b1; }</style></head>\n')
+html.write('<body>\n')
+html.write('<img src="map-edited.svg" alt="Map" />\n')
+html.write('<table class="sortable" style="border-collapse: collapse; width: 680px;">\n')
+html.write('<tr><th>Country</th><th>Name</th><th>Average incidence<br />in the last {0} days</th><th>Change in<br />the last {0} days</th></tr>\n'.format(days))
 
 for country in countries:
-    pop = get_population(country)
+    pop = pop_data.query('Code == "{0}"'.format(country)).iloc[0]['Population']
     last_cases = []
     for backlook in range(0, days):
-        cases = get_cases(data, country, backlook)
+        cases = get_cases(country, backlook)
         last_cases.append(calc_incidence(cases, pop))
     last_cases.reverse()
     
     first = last_cases[0]
     average = sum(last_cases) / days
-    percent = average / first * 100 - 100
+    change = average / first * 100 - 100
     
-    changes[country] = percent
-
-changes = sorted(changes.items(), key=lambda x: x[1])
-
-svg = ET.parse('map.svg')
-root_node = svg.getroot()
-
-for country, change in changes:
-    print('Country: {0} change {1:+.0f} %'.format(country, change))
+    print('country {0}; average {2:.1f}; change {1:+.0f} %'.format(country, change, average))
     country_node = root_node.findall('.//*[@id="{0}"]'.format(country.lower()))[0]
     style = country_node.attrib['style']
     
@@ -133,20 +74,11 @@ for country, change in changes:
         country_node.attrib['style'] = style.replace('#c0c0c0', red)
     elif change < 0:
         country_node.attrib['style'] = style.replace('#c0c0c0', green)
+    
+    name = pop_data.query('Code == "{0}"'.format(country)).iloc[0]['Name']
+    html.write('<tr><td>{0}</td><td>{1}</td><td>{2:.1f}</td><td sorttable_customkey="{3:.0f}">{3:+.0f} %</td></tr>\n'.format(country, name, average, change))
 
 svg.write('map-edited.svg')
-
-html = open('results.html', 'w')
-html.write('<!doctype html>\n')
-html.write('<html lang="en">\n')
-html.write('<head><title>-</title><script src="sorttable.js"></script><style>td { border: 1px solid #a2a9b1; }</style></head>\n')
-html.write('<body>\n')
-html.write('<img src="map-edited.svg" alt="Map" />\n')
-html.write('<table class="sortable" style="border-collapse: collapse;">\n')
-html.write('<tr><th>Country</th><th>Change in the last {0} days</th></tr>\n'.format(days))
-
-for country, change in changes:
-    html.write('<tr><td>{0}</td><td sorttable_customkey="{1:.0f}">{1:+.0f} %</td></tr>\n'.format(country, change, change))
 
 html.write('</table>')
 html.write('</body></html>\n')
